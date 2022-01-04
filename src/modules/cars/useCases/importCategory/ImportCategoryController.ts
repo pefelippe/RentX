918b4219e9f95b403ba42c2d66250ddc6685c663
Pedro Felippe <pedfelippe@gmail.com>
2022-01-04 01:11:16 -0300

    criando container com postgres

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -6,7 +6,9 @@ class ImportCategoryController {
 
   handle(request: Request, response: Response): Response {
     const { file } = request;
-    this.importCategoryUseCase.execute(file);
+    if (file) {
+      this.importCategoryUseCase.execute(file);
+    }
     return response.status(201).send();
   }
 }
